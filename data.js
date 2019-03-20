a02bdc67aab3fb5fa8da0b963f954b84dfda206c
Ly <h_ly70268@tss-hr420-dm30.fc.ca>
2019-03-19 21:39:56 -0400

    updated

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 const data ={
     name:"Han Ly",
-    role: "Designer",
+    role:"Designer",
 
 user1:{
     name:"Tung Le",
     role: "Projectasas Manager",
     bio: "Manage the timesheet",
-    bioPic:"images/pooh.jpg"
+    bioPic:"pooh.jpg"
     },
 user2:{
     name:"Tung Le",
@@ -14,6 +14,6 @@ user2:{
     bio: "Manage the timesheet",
     bioPic:""
     }
-}
+};
 
 module.exports = data;
